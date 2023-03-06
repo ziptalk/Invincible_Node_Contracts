bb5a4757d959b4e7c5a049f3d3419148173036e7
hoony0321 <eogns0321@gmail.com>
2023-03-06 17:42:04 +0900

    feat : ERC20 Token contract

---
+++ contracts/ERC20Token.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERCToekn is ERC20 {
+    address owner;
+
+    constructor(string memory _tokenFullName, string memory _tokenName) ERC20( _tokenFullName, _tokenName) {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not authorized");
+        _;
+    }
+
+    function getOwner(address _account) onlyOwner external  {
+        owner = _account;
+    }
+    
+    function mintToken(address _account, uint _amount) onlyOwner external {
+        _mint(_account, _amount);
+    }
+
+    function burnToken(address _account, uint _amount) onlyOwner external  {
+        _burn(_account, _amount);
+    }
+}
