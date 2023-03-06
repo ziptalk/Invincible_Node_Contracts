6af73a3c3d99b1157a4d6571aeee1671fdf25744
juns98 <juns98@naver.com>
2023-03-06 21:02:20 +0900

    "feat: init invi core"

---
+++ contracts/InviCore.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract InviCore {
+
+    fallback() external payable {
+        // _stake();
+    }
+
+    // stake native coin
+    function _stake(address _sender, uint _amount) private {
+
+    }
+
+    // unStake native coin
+    function unStake() private {
+
+    }
+
+    function splitRewards(uint _amount) public {
+
+    }
+}
