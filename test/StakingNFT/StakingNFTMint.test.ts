c2e0ee9874f4cd74a6dc420550220906349ab233
juns98 <juns98@naver.com>
2023-03-08 21:53:46 +0900

    feat: add testing logic and transfer logic

--- test/StakingNFT/StakingNFTMint.test.ts
+++ test/StakingNFT/StakingNFTMint.test.ts
@@ -5,41 +5,61 @@ import Web3 from "web3";
 import { string } from "hardhat/internal/core/params/argumentTypes";
 
 describe("Staking NFT Test", function () {
+  // deploy contract
   async function deployFixture() {
-    const [deployer, user] = await ethers.getSigners();
-    console.log("addresses: ", deployer.address);
+    const [deployer, user1, user2] = await ethers.getSigners();
+    console.log("addresses: ", deployer.address, user1.address, user2.address);
     const StakingNFTContract = await ethers.getContractFactory("StakingNFT");
     const stakingNFTContract = await StakingNFTContract.deploy();
     await stakingNFTContract.deployed();
 
+    // expect deployer == owner
+    expect(await stakingNFTContract.owner()).to.equal(deployer.address);
     const web3 = new Web3();
-    return { web3, deployer, user, stakingNFTContract };
+    return { web3, deployer, user1, user2, stakingNFTContract };
   }
-  // it("Test Staking logic", async function(){
-  //     const { web3, deployer, user, reTokenContract, klaytnContract } = await deployFixture();
-  //     const transaction = {
-  //         "to" : klaytnContract.address,
-  //         "from" : user.address,
-  //         "value" : 0.001 * 10 ** 18,
-  //         "data" : web3.utils.toHex("50,50")
-  //     }
-  //     await user.call(transaction);
-  // })
+
+  // 1. test token minting
   it("Test minting logic", async function () {
-    const { web3, deployer, user, stakingNFTContract } = await deployFixture();
-    const data = await stakingNFTContract.functions.mintNFT(
-      user.toString(),
-      1000,
-      10000,
-      100000
+    const { web3, deployer, user1, user2, stakingNFTContract } =
+      await deployFixture();
+    const [principal, lockPeriod, expectedReward] = [1000, 10000, 100000];
+    // console.log(stakingNFTContract.functions.mintNFT);
+    await stakingNFTContract.functions.mintNFT(
+      user1.address,
+      principal,
+      lockPeriod,
+      expectedReward
     );
-    console.log(data);
+
+    const test = await stakingNFTContract.functions.balanceOf(user1.address);
+    console.log(test);
   });
-  // it("Test Staking logic2", async function(){
-  //     const { web3, deployer, user, reTokenContract, klaytnContract } = await deployFixture();
-  //     const withdrawAmount = ethers.BigNumber.from("1000000000000000000");
-  //     await reTokenContract.connect(user).mintToken(user.address, withdrawAmount);
-  //     await reTokenContract.connect(user).approve(klaytnContract.address, withdrawAmount);
-  //     await klaytnContract.connect(user).withdraw(withdrawAmount);
-  // })
+
+  // 2. test token transfer
+  it("Test token transfer logic", async function () {
+    const { web3, deployer, user1, user2, stakingNFTContract } =
+      await deployFixture();
+    const [principal, lockPeriod, expectedReward] = [1000, 10000, 100000];
+    const tokenID = 0;
+
+    // mint nft
+    await stakingNFTContract.functions.mintNFT(
+      user1.address,
+      principal,
+      lockPeriod,
+      expectedReward
+    );
+
+    // user 1 approve user 2 to use token
+    console.log(
+      await stakingNFTContract.connect(user1).approve(user2.address, tokenID)
+    );
+    // transfer NFT from user1 to user2
+    console.log(
+      await stakingNFTContract
+        .connect(user2)
+        .transferFrom(user1.address, user2.address, tokenID)
+    );
+  });
 });
