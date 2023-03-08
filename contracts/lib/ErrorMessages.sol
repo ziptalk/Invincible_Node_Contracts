d3e5f20516c607c64c5621a7646280d99d1b9b69
hoony0321 <eogns0321@gmail.com>
2023-03-08 21:54:37 +0900

    feat : add ErrorMessages & Structs

---
+++ contracts/lib/ErrorMessages.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+string constant ERROR_NOT_OWNER = "not authorized";
+string constant ERROR_INSUFFICIENT_BALANCE = "insufficient balance";
+string constant ERROR_FAIL_SEND = "failed to send";
