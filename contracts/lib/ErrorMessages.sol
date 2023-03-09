f891daaedc907fc7a65e52947c0bdacf4fde98a9
hoony0321 <eogns0321@gmail.com>
2023-03-09 17:02:28 +0900

    docs : add error msg & add dependency & modify config
    confg : default 삭제
    dependency : dotenv

--- contracts/lib/ErrorMessages.sol
+++ contracts/lib/ErrorMessages.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+// general error
 string constant ERROR_NOT_OWNER = "not authorized";
 string constant ERROR_INSUFFICIENT_BALANCE = "insufficient balance";
 string constant ERROR_FAIL_SEND = "failed to send";
+
+// stake info error
+string constant ERROR_INVALID_STAKE_INFO = "invalid stake info";
