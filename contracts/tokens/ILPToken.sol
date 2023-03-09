6dc117a76e617c8296a714a4ae346af6e3512223
juns98 <juns98@naver.com>
2023-03-09 16:54:31 +0900

    feat: made tokens upgradeable. added OwnableUpgradeable.sol

--- contracts/tokens/ILPToken.sol
+++ contracts/tokens/ILPToken.sol
@@ -1,28 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 string constant ILP_TOKEN_FULL_NAME = "Invi Liquidity Provider Token";
 string constant ILP_TOKEN_NAME = "ILP";
 
-contract ILPToken is ERC20 {
 
-    address public owner;
+contract ILPToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
 
-    constructor() ERC20(ILP_TOKEN_FULL_NAME, ILP_TOKEN_NAME) {
-        owner = msg.sender;
+    function initialize() initializer public {
+        __ERC20_init(ILP_TOKEN_FULL_NAME, ILP_TOKEN_NAME);
+        __Ownable_init();
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "not authorized");
-        _;
-    }
-
-    function setOwner(address _account) onlyOwner external {
-        owner = _account;
-    }
-    
     function mintToken(address _account, uint _amount) onlyOwner external {
         _mint(_account, _amount);
     }
