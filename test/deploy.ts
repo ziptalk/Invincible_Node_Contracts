b98475b2512f324b479a608baf6feb31ece2ac56
hoony0321 <eogns0321@gmail.com>
2023-03-13 17:02:20 +0900

    test : inviCore contract test
    stake() function test

---
+++ test/deploy.ts
@@ -0,0 +1,48 @@
+import { Contract, Wallet } from "ethers";
+import { ethers, upgrades } from "hardhat";
+
+// deploy InviToken contract
+export const deployInviToken = async () => {
+    const InviTokenContract = await ethers.getContractFactory("InviToken");
+    const inviTokenContract = await upgrades.deployProxy(InviTokenContract, [], {initializer: "initialize",});
+    await inviTokenContract.deployed();
+
+    return inviTokenContract;
+}
+
+// deploy ILPToken contract
+export const deployILPToken = async () => {
+    const ILPTokenContract = await ethers.getContractFactory("ILPToken");
+    const iLPTokenContract = await upgrades.deployProxy(ILPTokenContract, [], {initializer: "initialize",});
+    await iLPTokenContract.deployed();
+
+    return iLPTokenContract;
+}
+
+// deploy stakeNFT contract
+export const deployStakeNFT = async () => {
+    const StakeNFTContract = await ethers.getContractFactory("StakeNFT");
+    const stakeNFTContract = await upgrades.deployProxy(StakeNFTContract, [], {initializer: "initialize",});
+    await stakeNFTContract.deployed();
+
+    return stakeNFTContract;
+}
+
+// deploy lpPool contract
+export const deployLpPoolContract = async (iLPTokenContract : Contract, inviTokenContract : Contract) => {
+    const LpPoolContract = await ethers.getContractFactory("LiquidityProviderPool");
+    const lpPoolContract = await upgrades.deployProxy(LpPoolContract, [iLPTokenContract.address, inviTokenContract.address], {initializer: "initialize",});
+    await lpPoolContract.deployed();
+
+    return lpPoolContract;
+}
+
+// deploy inviCore contract
+export const deployInviCoreContract = async (stakeManager : string, stakeNFTContract : Contract, lpPoolContract : Contract) => {
+    
+    const InviCoreContract = await ethers.getContractFactory("InviCore");
+    const inviCoreContract = await upgrades.deployProxy(InviCoreContract, [stakeManager, stakeNFTContract.address, lpPoolContract.address], {initializer: "initialize",});
+    await inviCoreContract.deployed();
+
+    return inviCoreContract;
+}
