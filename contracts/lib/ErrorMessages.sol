9f7a4f4d8450bbdbc81a4f48b616a79931d420ee
juns98 <64398993+juns98@users.noreply.github.com>
2023-03-18 00:41:49 +0900

    feat: add error messages

--- contracts/lib/ErrorMessages.sol
+++ contracts/lib/ErrorMessages.sol
@@ -10,3 +10,9 @@ string constant ERROR_FAIL_SEND = "failed to send";
 string constant ERROR_INVALID_STAKE_INFO = "invalid stake info";
 string constant ERROR_NOT_OWNED_NFT = "not your own nft";
 string constant ERROR_TOO_MUCH_LENT = "too much lent amount";
+string constant ERROR_NOT_EXPIRED_NFT = "nft is still locked";
+
+// setter error
+string constant ERROR_SET_REWARD_PORTION = "lp pool portion + invi stake portion should equal to rewardPortionTotalUnit";
+string constant ERROR_NFT_REWARD_CRASH = "initial nft reward should be zero";
+string constant ERROR_NFT_REWARD_INVALID_RANGE = "nft reward should be between min reward and max reward";
