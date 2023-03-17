5b488a1e009565da3143133346cc987180cda890
juns98 <64398993+juns98@users.noreply.github.com>
2023-03-18 00:52:51 +0900

    feat: added units

--- contracts/lib/Unit.sol
+++ contracts/lib/Unit.sol
@@ -7,4 +7,5 @@ uint constant protocolFeeUnit = 10 ** 3;
 uint constant slippageUnit = 10 ** 3;
 uint constant aprUnit = 10 ** 2;
 uint constant rewardErrorUnit = 10 ** 3;
-uint constant liqudityAllowableRatioUnit = 10 ** 2;
+uint constant liquidityAllowableRatioUnit = 10 ** 2;
+uint constant rewardPortionTotalUnit = 10 ** 3;
