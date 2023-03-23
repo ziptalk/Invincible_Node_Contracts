bca94900d9bd1fff18bf34c06a78ea96be7779d5
hoony0321 <eogns0321@gmail.com>
2023-03-23 17:58:56 +0900

    test : contract's main services

--- test/LiquidityProviderPool/services.test.ts
+++ test/LiquidityProviderPool/services.test.ts
@@ -1,70 +1,74 @@
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 import { BigNumber, Contract } from "ethers";
-import { deployInviToken, deployILPToken, deployLpPoolContract } from "../deploy";
+import { deployInviToken, deployILPToken, deployLpPoolContract, deployAllWithSetting} from "../deploy";
+import { provideLiquidity } from "../utils";
 
 describe("Liquidity Provider Pool Test", function () {
+  let stKlayContract: Contract;
+  let inviCoreContract: Contract;
+  let stakeNFTContract: Contract;
   let lpPoolContract: Contract;
   let iLPTokenContract: Contract;
   let inviTokenContract: Contract;
+  let inviTokenStakeContract: Contract;
 
   this.beforeEach(async () => {
-    const [deployer, stakeManager, LP, userA, userB, userC] = await ethers.getSigners();
-
-    // deploy ILPToken contract
-    iLPTokenContract = await deployILPToken();
-    // deploy inviToken contract
-    inviTokenContract = await deployInviToken();
-    // deploy liquidity pool contract
-    lpPoolContract = await deployLpPoolContract(stakeManager.address, iLPTokenContract, inviTokenContract);
-    // change ILPToken owner
-    await iLPTokenContract.connect(deployer).transferOwnership(lpPoolContract.address);
-    // change inviToken owner
-    await inviTokenContract.connect(deployer).transferOwnership(lpPoolContract.address);
+    [stKlayContract, inviCoreContract, iLPTokenContract, stakeNFTContract, inviTokenContract, lpPoolContract, inviTokenStakeContract] = await deployAllWithSetting();
   });
 
-  const stakeAmountEther: string = "1";
-  it("Test Stake", async function () {
-    console.log("-------------------Test Stake-------------------");
+  it("Test LP Stake", async function () {
     const [deployer, stakeManager, LP, userA, userB, userC] = await ethers.getSigners();
-    await lpPoolContract.connect(userA).stake({ value: ethers.utils.parseEther(stakeAmountEther) });
 
-    // expect totalStakedAmount == userAStakedAmount == amount
-    const expecteduserAReward = ethers.utils.parseEther(stakeAmountEther);
-    const totalStakedAmount = await lpPoolContract.functions.totalStakedAmount();
-    const userAStakedAmount = await lpPoolContract.functions.stakedAmount(userA.address);
-    expect(totalStakedAmount.toString()).to.equal(expecteduserAReward.toString());
-    expect(userAStakedAmount.toString()).to.equal(expecteduserAReward.toString());
+    //* given
+    const userAStakedAmount = 100000;
+    const userBStakedAmount = 200000;
+    const userCStakedAmount = 300000;
+
+    //* when
+    await provideLiquidity(lpPoolContract, userA, userAStakedAmount); // lp stake
+    await provideLiquidity(lpPoolContract, userB, userBStakedAmount); // lp stake
+    await provideLiquidity(lpPoolContract, userC, userCStakedAmount); // lp stake
+
+    //* then
+    expect(await lpPoolContract.totalStakedAmount()).to.equal(userAStakedAmount + userBStakedAmount + userCStakedAmount);
+    expect(await lpPoolContract.stakedAmount(userA.address)).to.equal(userAStakedAmount);
+    expect(await lpPoolContract.stakedAmount(userB.address)).to.equal(userBStakedAmount);
+    expect(await lpPoolContract.stakedAmount(userC.address)).to.equal(userCStakedAmount);
   });
 
   it("Test distribute Reward", async function () {
-    console.log("----------------Test Update Reward----------------");
     const [deployer, stakeManager, LP, userA, userB, userC] = await ethers.getSigners();
-    const stakeAmount: Number = 1000000;
-    const rewardAmountEther: string = "3";
 
-    // stake from multiple addresses
-    // user 1
-    await lpPoolContract.connect(userA).stake({ value: ethers.utils.parseEther(stakeAmountEther) });
-    // user 2
-    await lpPoolContract.connect(userB).stake({ value: ethers.utils.parseEther(stakeAmountEther) });
-    // user 3
-    await lpPoolContract.connect(userC).stake({ value: ethers.utils.parseEther(stakeAmountEther) });
+    //* given
+    const totalStakedAmount = 1000000;
+    const userAStakedAmount = Math.floor(totalStakedAmount * 0.2);
+    const userBStakedAmount = Math.floor(totalStakedAmount * 0.3);
+    const userCStakedAmount = Math.floor(totalStakedAmount * 0.5);
 
-    const totalStakedAmount = await lpPoolContract.functions.totalStakedAmount();
-    const userAStakedAmount = await lpPoolContract.functions.stakedAmount(userA.address);
-    console.log(totalStakedAmount.toString());
+    await provideLiquidity(lpPoolContract, userA, userAStakedAmount); // lp stake
+    await provideLiquidity(lpPoolContract, userB, userBStakedAmount); // lp stake
+    await provideLiquidity(lpPoolContract, userC, userCStakedAmount); // lp stake
 
-    // distribute Reward to stakers
-    await lpPoolContract
-      .connect(stakeManager)
-      .distributeNativeReward({ value: ethers.utils.parseEther(rewardAmountEther) })
-      .catch((error: any) => {
-        console.error(error);
-      });
+    const initUserABalance = await userA.getBalance();
+    const initUserBBalance = await userB.getBalance();
+    const initUserCBalance = await userC.getBalance();
 
-    // user inviToken balance should not be 0
-    const userAInviBalance = await inviTokenContract.functions.balanceOf(userA.address);
-    expect(userAInviBalance).to.not.equal(0);
+
+    //* when
+    const rewardAmount = 100000;
+    const tx = await deployer.sendTransaction({ to: inviCoreContract.address, value: rewardAmount,  gasLimit: 300000}); // send coin with tx fee to contract
+    
+    await inviCoreContract.connect(deployer).createUnstakeRequest(lpPoolContract.address, rewardAmount, 0, 1); // create unstake request
+    await inviCoreContract.connect(stakeManager).sendUnstakedAmount(); // send coin to lpPoolContract
+
+    //* then
+    expect(await inviCoreContract.getUnstakeRequestsLength()).to.equal(0);
+    expect(await lpPoolContract.nativeRewardAmount(userA.getAddress())).to.equal(rewardAmount * 0.2);
+    expect(await lpPoolContract.nativeRewardAmount(userB.getAddress())).to.equal(rewardAmount * 0.3);
+    expect(await lpPoolContract.nativeRewardAmount(userC.getAddress())).to.equal(rewardAmount * 0.5);
+    expect(await userA.getBalance()).to.equal(initUserABalance.add(rewardAmount * 0.2));
+    expect(await userB.getBalance()).to.equal(initUserBBalance.add(rewardAmount * 0.3));
+    expect(await userC.getBalance()).to.equal(initUserCBalance.add(rewardAmount * 0.5));
   });
-});
+})
