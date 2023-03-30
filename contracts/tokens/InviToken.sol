4cc6ff0124ca9cd06dd23e1d5cb5b5b621b6c7ca
hoony0321 <eogns0321@gmail.com>
2023-03-30 18:05:15 +0900

    feat : add mintLentToken & burnLentToken

--- contracts/tokens/InviToken.sol
+++ contracts/tokens/InviToken.sol
@@ -9,10 +9,23 @@ string constant INVI_TOKEN_FULL_NAME = "Invi Token";
 string constant INVI_TOKEN_NAME = "INVI";
 
 contract InviToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    address public LEND_INVI_TOKEN;
+
     //====== initializer ======//
     function initialize() initializer public {
         __ERC20_init(INVI_TOKEN_FULL_NAME, INVI_TOKEN_NAME);
         __Ownable_init();
+    }
+
+    //====== modifiers ======//
+    modifier onlyLendInviToken {
+        require(msg.sender == address(LEND_INVI_TOKEN), "msg sender should be lend invi token");
+        _;
+    }
+
+    //====== setter functions ======//
+    function setLendInviToken(address _lendInviToken) onlyOwner external {
+        LEND_INVI_TOKEN = _lendInviToken;
     }
 
     //====== service functions ======//
@@ -20,7 +33,15 @@ contract InviToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
         _mint(_account, _amount);
     }
 
+    function mintLentToken(address _account, uint _amount) onlyLendInviToken external {
+        _mint(_account, _amount);
+    }
+
     function burnToken(address _account, uint _amount) onlyOwner external  {
+        _burn(_account, _amount);
+    }
+
+    function burnLentToken(address _account, uint _amount) onlyLendInviToken external  {
         _burn(_account, _amount);
     }
 }
