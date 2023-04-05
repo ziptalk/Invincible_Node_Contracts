a8fb1cbec26adab129988e435f79500ff3a1c94e
juns98 <juns98@naver.com>
2023-04-05 18:43:38 +0900

    feat: update test code

---
+++ test/SwapManager/services.test.ts
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers, network, upgrades } from "hardhat";
+import { deployAllWithSetting } from "../deploy";
+
+const { expectRevert } = require("@openzeppelin/test-helpers");
+
+describe("SwapManager Service Test", function () {
+  let swapManagerContract: Contract;
+
+  this.beforeEach(async () => {
+    ({ swapManagerContract } = await deployAllWithSetting());
+  });
+
+  it("Test Fetch Price functions", async () => {
+    const [deployer, stakeManager, userA, userB, userC] = await ethers.getSigners();
+    const price = await swapManagerContract.functions.fetchKlayPrice();
+    console.log(price);
+  });
+});
