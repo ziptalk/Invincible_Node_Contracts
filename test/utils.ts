5a8223c3b40f102404c8e51fb4ec24dcdebb5dde
juns98 <juns98@naver.com>
2023-04-20 18:09:31 +0900

    feat: update getStakeInfo function to include lockPeriod

--- test/utils.ts
+++ test/utils.ts
@@ -5,19 +5,20 @@ import units from "./units.json";
 type SignerWithAddress = ethers.Signer & { getAddress: () => Promise<string> };
 
 export const provideLiquidity = async (lpPoolContract: Contract, user: SignerWithAddress, amount: number) => {
-    await lpPoolContract.connect(user).stake({ value: amount });
-}
+  await lpPoolContract.connect(user).stake({ value: amount });
+};
 
-export const leverageStake = async (inviCoreContract: Contract, user: SignerWithAddress, principal: number, leverageRatio: number) => {
-    const stakeInfo = await inviCoreContract.connect(user).getStakeInfo(await user.getAddress(), principal, leverageRatio);
-    const slippage = 3 * units.slippageUnit;
-    await inviCoreContract.connect(user).stake(stakeInfo, slippage, { value: principal });
-    return stakeInfo;
-}
+export const leverageStake = async (inviCoreContract: Contract, user: SignerWithAddress, principal: number, leverageRatio: number, lockPeriod: number) => {
+  const stakeInfo = await inviCoreContract.connect(user).getStakeInfo(await user.getAddress(), principal, leverageRatio, lockPeriod);
+  const slippage = 3 * units.slippageUnit;
+  console.log(stakeInfo);
+  await inviCoreContract.connect(user).stake(stakeInfo, slippage, { value: principal });
+  return stakeInfo;
+};
 
-export const verifyRequest = async (request : any, recipient : string, amount : number, fee : number, type : number) => {
-    expect(request.recipient).to.equal(recipient);
-    expect(request.amount).to.equal(amount);
-    expect(request.fee).to.equal(fee);
-    expect(request.requestType).to.equal(type);
-}
+export const verifyRequest = async (request: any, recipient: string, amount: number, fee: number, type: number) => {
+  expect(request.recipient).to.equal(recipient);
+  expect(request.amount).to.equal(amount);
+  expect(request.fee).to.equal(fee);
+  expect(request.requestType).to.equal(type);
+};
