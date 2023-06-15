9a31cf28d1f2dbfd03e48c17377ea0c2eaf55589
juns98 <juns98@naver.com>
2023-06-15 15:57:04 +0900

    refactor: claim style

--- contracts/Evmos/EvmosInviCore.sol
+++ contracts/Evmos/EvmosInviCore.sol
@@ -51,6 +51,8 @@ contract EvmosInviCore is Initializable, OwnableUpgradeable {
 
     //------upgrades------//
     mapping (uint => uint) public nftUnstakeTime;
+    mapping (address => uint) public claimableAmount;
+
 
     //======initializer======//
     function initialize(address _stTokenAddr, address _evmosLiquidStakingAddr) initializer public {
@@ -325,16 +327,29 @@ contract EvmosInviCore is Initializable, OwnableUpgradeable {
             address recipient = unstakeRequests[i].recipient;
             // remove first element of unstakeRequests
             unstakeRequestsFront = dequeueUnstakeRequests(unstakeRequests, unstakeRequestsFront, unstakeRequestsRear);
+            // if normal user
             if (requestType == 0) {
-                (bool sent, ) = recipient.call{value : amount }("");
-                require(sent, ERROR_FAIL_SEND);
-            } else if (requestType == 1) {
+                claimableAmount[recipient] += amount;
+            } 
+            // if lp pool
+            else if (requestType == 1) {
                 lpPoolContract.distributeNativeReward{value : amount }();
-            } else if (requestType == 2) {
+            } 
+            // if invi token stake
+            else if (requestType == 2) {
                 inviTokenStakeContract.updateNativeReward{value : amount }();
             }
         }
     } 
+
+    // claim unstaked amount
+    function claimUnstaked() external {
+        require(claimableAmount[msg.sender] > 0, ERROR_NO_CLAIMABLE_AMOUNT);
+        uint amount = claimableAmount[msg.sender];
+        claimableAmount[msg.sender] = 0;
+        (bool sent, ) = msg.sender.call{value : amount }("");
+        require(sent, ERROR_FAIL_SEND);
+    }
     
     //====== utils function ======//
     // verify stakeInfo is proper
