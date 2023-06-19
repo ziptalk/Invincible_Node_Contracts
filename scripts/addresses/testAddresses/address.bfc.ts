64506697b5a6a68fc00282764c47d623a4501a2f
juns98 <juns98@naver.com>
2023-06-19 18:11:10 +0900

    feat: update evmos

--- scripts/addresses/testAddresses/address.bfc.ts
+++ scripts/addresses/testAddresses/address.bfc.ts
@@ -4,16 +4,16 @@ export const testAddressTestnetBfc = {
   bfcLiquidStaking: "0x3C19636e4796debAE3FfDFe287B18e02593e3b44",
   stBfc: "0x0fB009bF866fC9B2924b7eF214B084348Cf9e93a",
 
-  inviTokenContractAddress: "0xc9e8761617d0C1c0e31E16DaaE74C3cF3B225E61",
-  iLPTokenContractAddress: "0xe09f07Cf069dFe8646e20E25e04bc00121e38787",
-  iSPTTokenContractAddress: "0x93d7F10165c86F63cC8244963cF89392245Dd6f9",
-  stakeNFTContractAddress: "0x5a7Ce770924Dfd78288948ACf3a48bfA2932D314",
-  inviTokenStakeContractAddress: "0x6094942b882b8560c036c837863354B91fc122f9",
-  lpPoolContractAddress: "0x5e63a168b91b9a41760B896cE51Ee37be82AfF3a",
-  lendingPoolContractAddress: "0xB38AE6EA2f52894A9E4B3983e15570A3235f0a62",
-  inviSwapPoolContractAddress: "0xac9f0E95FfA65cE1E819e9a40Bcca4e4D5dd9279",
-  inviCoreContractAddress: "0xa166434299DD2be3cc2f28Bc530E4f3129d8181e",
-  priceManagerContractAddress: "0xDb4F8F1F0a2172Bb3a5eC91f27563bA099dd9d5d",
+  inviTokenContractAddress: "0x166370666b59386D46c30abD20962896c764E651",
+  iLPTokenContractAddress: "0xA86B7A43962974Cf95352F28f85C6F4c48cc4cEa",
+  iSPTTokenContractAddress: "0x462f8A552CD255355bE294f8431dc009a1eaC705",
+  stakeNFTContractAddress: "0x06a22DA90Cfcb0f9B434f0fC834c6255bBB111ca",
+  inviTokenStakeContractAddress: "0xC4dfA3179810aA895c925C34bB2d7DEbD9D50D42",
+  lpPoolContractAddress: "0x5ECaA30B42CfcE542664fd3B274fF41e2078F3d9",
+  lendingPoolContractAddress: "0x851da3d9f8D760254ad893269354bE6ddDF9585c",
+  inviSwapPoolContractAddress: "0xa322b9E9e1b6F14Dd5658D6cEA9054E3276fadc9",
+  inviCoreContractAddress: "0x5120b562453EC5588D01bc06C5806C4ed84fd3c9",
+  priceManagerContractAddress: "0x22ad3B981105cFF6B1b7B49600d05a4203AfD659",
 };
 
 export const testAddressMainnetBfc = {};
