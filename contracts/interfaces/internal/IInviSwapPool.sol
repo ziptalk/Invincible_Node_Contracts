05d838ca0db667637e62148f1e256d6fd8fa0d5f
juns98 <juns98@naver.com>
2023-06-27 16:11:59 +0900

    feat: update interface

--- contracts/interfaces/internal/IInviSwapPool.sol
+++ contracts/interfaces/internal/IInviSwapPool.sol
@@ -2,19 +2,18 @@
 pragma solidity ^0.8.0;
 
 interface IInviSwapPool {
-    // Getter Functions
     function getInviToNativeOutAmount(uint _amountIn) external view returns (uint);
     function getNativeToInviOutAmount(uint _amountIn) external view returns (uint);
-    function getInviToNativeOutMaxInput() external view returns (uint);
     function getNativeToInviOutMaxInput() external view returns (uint);
-    
-    // Setter Functions
-    function setPriceManager(address _priceManager) external;
-    function setInviFees(uint _fees) external;
-    function setNativeFees(uint _fees) external;
-    
-    // Service Functions
+    function getInviToNativeOutMaxInput() external view returns (uint);
+    function getAddLiquidityInvi(uint _amountIn) external view returns (uint);
+    function getAddLiquidityNative(uint _amountIn) external view returns (uint);
+    function getInviPrice() external view returns (uint);
+    function getNativePrice() external view returns (uint);
+    function getExpectedAmountsOutRemoveLiquidity(uint _liquidityTokensAmount) external view returns (uint inviAmount, uint nativeAmount);
     function swapInviToNative(uint _amountIn, uint _amountOutMin) external;
     function swapNativeToInvi(uint _amountOutMin) external payable;
+    function addLiquidity(uint _expectedAmountInInvi, uint _slippage) external payable;
+    function removeLiquidity(uint _liquidityTokensAmount, uint _expectedInviAmount, uint _expectedNativeAmount, uint _slippage) external;
     function withdrawFees() external;
 }
